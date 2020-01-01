84891b6ca89acc931510078477bbbb3b3810b808
Hartuty <atuti.brett@gmail.com>
2020-01-01 12:53:42 +0300

    Home Sceen

---
+++ app/src/main/java/com/abach/iptv/Stream.java
@@ -0,0 +1,14 @@
+package com.abach.iptv;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Stream extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_stream);
+    }
+}
